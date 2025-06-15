6275e37d53c51f73a919e819c66afb634b678d3c
grant <abernathy.grant@icloud.com>
2025-06-14 20:23:34 -0500

    Implementation start

---
+++ Source/Core/EdgeMemory.cpp
@@ -0,0 +1,16 @@
+/*
+ * EdgeMemory.cpp
+ *
+ * Grant Abernathy
+ *
+ * 06-14-2025
+ *
+ * Memory management system.
+ *
+ * Responsibilities:
+ * - To provide memory allocation interfaces and utilities,
+ * - provide support for memory tracking and debugging,
+ * - Provide platform-specific memory optimizations,
+ * - Provide alignment utilities for different data types,
+ * - And provide memory pools and arena allocators.
+*/
